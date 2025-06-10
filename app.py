00025eb210fbf4341b64bf205d9657c09f8072bb
Eduardokorb2023 <148217028+Eduardokorb2023@users.noreply.github.com>
2025-06-10 19:36:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import string
 from IPython.display import display, HTML
 import ipywidgets as widgets
 
-# Função geradora de senhas
 def gerar_senha(comprimento, usar_maiusculas, usar_minusculas, usar_numeros, usar_simbolos):
     caracteres = ""
     if usar_maiusculas:
@@ -20,7 +19,6 @@ def gerar_senha(comprimento, usar_maiusculas, usar_minusculas, usar_numeros, usa
 
     return ''.join(random.choice(caracteres) for _ in range(comprimento))
 
-# Widgets de entrada
 comprimento_slider = widgets.IntSlider(value=12, min=4, max=32, description='Comprimento:')
 maiusculas_check = widgets.Checkbox(value=True, description='Maiúsculas')
 minusculas_check = widgets.Checkbox(value=True, description='Minúsculas')
@@ -30,7 +28,6 @@ simbolos_check = widgets.Checkbox(value=False, description='Símbolos')
 botao_gerar = widgets.Button(description='Gerar Senha')
 saida = widgets.Output()
 
-# Função acionada ao clicar no botão
 def ao_clicar_botao(b):
     senha = gerar_senha(
         comprimento_slider.value,
@@ -45,7 +42,6 @@ def ao_clicar_botao(b):
 
 botao_gerar.on_click(ao_clicar_botao)
 
-# Exibir interface
 display(widgets.VBox([
     comprimento_slider,
     widgets.HBox([maiusculas_check, minusculas_check, numeros_check, simbolos_check]),
